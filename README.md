d648ac1f161081d240930ec52451d62d2f90f490
fangyh09 <fangyh09@gmail.com>
2018-02-22 18:53:23 +0800

    add sh

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # gpu-status
 Show each user gpu usage under each gpu, provide more details than nvidia-smi
+
+# Install
---
+++ gpu-status.sh
@@ -0,0 +1,53 @@
+#!/bin/bash
+# author fangyh
+
+num_gpus=`lspci | grep VGA | grep 'NVIDIA' | wc -l`
+lines_date=7
+pre_lines=$(( $lines_date + $num_gpus * 3 + 6))
+lines=${pre_lines},"$"p
+
+gpus=`nvidia-smi| sed -n "$lines" | head -n -1 | awk '{print $2}'`
+pids=`nvidia-smi| sed -n "$lines" | head -n -1 | awk '{print $3}'`
+mems=`nvidia-smi| sed -n "$lines" | head -n -1 | awk '{print $(NF-1)}'`
+
+names_arr=()
+gpus_arr=()
+mems_arr=()
+pids_arr=()
+
+for gpu in $gpus;
+do
+	gpus_arr+=($gpu)
+done
+
+for mem in $mems;
+do
+	mems_arr+=($mem)
+done
+
+for pid in $pids;
+do
+	res=`ps aux | awk '!/grep/' | grep $pid`
+	name=`echo $res | awk '$2=$pid{print $1}'`
+	names_arr+=($name)
+	pids_arr+=($pid)
+done
+
+len=${#names_arr[@]}
+pre_id="0"
+printf "%0.s-" {1..51}
+printf "\n"
+printf "%12s %12s %12s %12s\n"  names pid gpus_id memm
+printf "%0.s-" {1..51}
+printf "\n"
+
+for ((i=0;i<len;i++));
+do
+	printf "%12s %12s %12s %12s\n" ${names_arr[i]} ${pids_arr[i]}   ${gpus_arr[i]}  ${mems_arr[i]}
+	if [ "${gpus_arr[i+1]}" != "$pre_id" ]  ; 
+	then
+		printf "%0.s-" {1..51}
+		printf "\n"
+		pre_id=${gpus_arr[i+1]}
+	fi
+done
